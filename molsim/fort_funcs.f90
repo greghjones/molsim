912af7fc053c6ef506fa28f26876fd5abdda08fd
Greg Jones <ghjones@mit.edu>
2026-08-06 14:46:57 -0400

    Be a bit more pedantic in Fortran bindings

--- molsim/fort_funcs.f90
+++ molsim/fort_funcs.f90
@@ -1,48 +1,58 @@
 subroutine make_gauss(centers, taus, l_idxs, u_idxs, freq_profile, tau_profile, npeaks, profilelength, dV, ckm)
+    use, intrinsic :: iso_c_binding, only: c_double, c_long_long
     implicit none
-    integer*8, intent(in) :: npeaks, profilelength
-    double precision, intent(in) :: dV, ckm
-    integer*8, intent(in) :: l_idxs(npeaks), u_idxs(npeaks)
-    double precision, intent(in) :: centers(npeaks), taus(npeaks), freq_profile(profilelength)
-    double precision, intent(out) :: tau_profile(profilelength)
+    
+    integer, parameter :: dp = c_double
 
-    integer*8 i, ll, ul
-    double precision scale1, scale2, f0
+    integer(c_long_long), intent(in) :: npeaks, profilelength
+    real(dp), intent(in) :: dV, ckm
+    integer(c_long_long), intent(in) :: l_idxs(npeaks), u_idxs(npeaks)
+    real(dp), intent(in) :: centers(npeaks), taus(npeaks), freq_profile(profilelength)
+    real(dp), intent(out) :: tau_profile(profilelength)
 
-    scale1 = 2.0d0*(dV/ckm/2.35482d0)**2.0d0
+    integer(c_long_long) i, ll, ul
+    real(dp) scale1, scale2, f0
+
+    real(dp), parameter :: two = 2.0_dp
+
+    scale1 = two*(dV/ckm/2.35482_dp)**two
 
     do i=1,npeaks
         ll = l_idxs(i)
         ul = u_idxs(i)
         f0 = centers(i)
-        scale2 = -1.0d0 / (scale1*f0*f0)
-        tau_profile(ll:ul) = tau_profile(ll:ul) + taus(i)*exp(scale2*(freq_profile(ll:ul) - f0)**2.0d0)
+        scale2 = -1.0_dp / (scale1*f0*f0)
+        tau_profile(ll:ul) = tau_profile(ll:ul) + taus(i)*exp(scale2*(freq_profile(ll:ul) - f0)**two)
     end do
     return
 end subroutine make_gauss
 
 subroutine calc_tb(freq_profile, tau_profile, tbg_profile, tex, h, k, tb_profile, profilelength, texlength)
+    use, intrinsic :: iso_c_binding, only: c_double, c_long_long
     implicit none
-    integer*8, intent(in) :: profilelength, texlength
-    double precision, intent(in) :: h, k
-    double precision, intent(in) :: freq_profile(profilelength), tau_profile(profilelength), tbg_profile(profilelength), tex(texlength)
-    double precision, intent(out) :: tb_profile(profilelength)
-
-    integer*8 i
-
-    double precision texv, scale, temp1, j_t, j_tbg
 
     interface
         real(c_double) function expm1(x) bind(c, name='expm1')
-            use, intrinsic :: iso_c_binding, only: c_double
+            import, only : c_double
             real(c_double), intent(in), value :: x
         end function expm1
     end interface
 
+    integer, parameter :: dp = c_double
+
+    integer(c_long_long), intent(in) :: profilelength, texlength
+    real(dp), intent(in) :: h, k
+    real(dp), intent(in) :: freq_profile(profilelength), tau_profile(profilelength), tbg_profile(profilelength), tex(texlength)
+    real(dp), intent(out) :: tb_profile(profilelength)
+
+    integer(c_long_long) i
+
+    real(dp) texv, scale, temp1, j_t, j_tbg
+
     scale = h*1.0d6/k
 
     if(texlength.eq.1)then
-        texv = 1.0d0/tex(1)
+        texv = 1.0_dp/tex(1)
         do i=1,profilelength
             temp1 = scale*freq_profile(i)
             j_t   = temp1 / expm1(texv*temp1)
@@ -61,28 +71,32 @@ subroutine calc_tb(freq_profile, tau_profile, tbg_profile, tex, h, k, tb_profile
 end subroutine calc_tb
 
 subroutine calc_tau(aij, gup, eup, frequencies, columndensity, tex, dV, q, h, k, cm, tau, ntransitions)
+    use, intrinsic :: iso_c_binding, only: c_double, c_long_long
     implicit none
-    integer*8, intent(in) :: ntransitions
-    double precision, intent(in) :: columndensity, tex, dV, q, h, k, cm
-    double precision, intent(in) :: aij(ntransitions), gup(ntransitions), eup(ntransitions), frequencies(ntransitions)
-    double precision, intent(out) :: tau(ntransitions)
-
-    double precision, parameter :: pi = 3.141592653589793238462643383279502884197d0
-
-    double precision :: prefactor, texinv, boltzmannscale
-    double precision :: invfcubed(ntransitions), exp1(ntransitions), exp2(ntransitions)
-    integer*8 i
 
     interface
         real(c_double) function expm1(x) bind(c, name='expm1')
-            use, intrinsic :: iso_c_binding, only: c_double
+            import, only : c_double
             real(c_double), intent(in), value :: x
         end function expm1
     end interface
 
-    texinv = -1.0d0/tex
+    integer, parameter :: dp = c_double
+
+    integer(c_long_long), intent(in) :: ntransitions
+    real(dp), intent(in) :: columndensity, tex, dV, q, h, k, cm
+    real(dp), intent(in) :: aij(ntransitions), gup(ntransitions), eup(ntransitions), frequencies(ntransitions)
+    real(dp), intent(out) :: tau(ntransitions)
+
+    real(dp), parameter :: pi = 3.141592653589793238462643383279502884197_dp
+
+    real(dp) :: prefactor, texinv, boltzmannscale
+    real(dp) :: invfcubed(ntransitions), exp1(ntransitions), exp2(ntransitions)
+    integer(c_long_long) i
+
+    texinv = -1.0_dp/tex
     boltzmannscale = h*1.0d6/(k*tex)
-    prefactor = log(2.0d0)**0.5d0 * cm*cm*cm * (columndensity * 100.0d0*100.0d0) / (4*pi**1.5d0 * 1.0d18 * dV*1000.0d0 * q)
+    prefactor = log(2.0_dp)**0.5_dp * cm*cm*cm * (columndensity * 100.0_dp*100.0_dp) / (4*pi**1.5_dp * 1.0d18 * dV*1000.0_dp * q)
 
     exp1 = exp(texinv*eup)
     do i = 1,ntransitions
